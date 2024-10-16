1741e47e329c48357909df1773841f7beadcefee
Jakob Osterberger <s2010456022@students.fh-hagenberg.at>
2024-10-16 20:54:39 +0200

    configure package

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
 # Changelog
 
-All notable changes to `:package_name` will be documented in this file.
+All notable changes to `laravel-collection-trend` will be documented in this file.
--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace VendorName\Skeleton\Database\Factories;
+namespace JkOster\CollectionTrend\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
